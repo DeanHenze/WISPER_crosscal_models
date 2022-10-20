22a510d518b70f72c86c76390694267234df3104
Dean Henze <deanchenze@gmail.com>
2022-10-20 15:12:01 -0700

    Modified documentation to qxcal_model.py

--- qxcal_model.py
+++ qxcal_model.py
@@ -3,6 +3,8 @@
 Created on Thu Oct 20 14:45:12 2022
 
 @author: Dean
+
+Model fit parameters for vapor concentration of sensor 2 vs sensor 1. 
 """
 
 
@@ -10,9 +12,7 @@ import statsmodels.api as sm # 0.12.0
 
 
 def modelfit(df, k_s1, k_s2):
-    """
-    Model fit parameters for vapor concentration of sensor 2 vs sensor 1. 
-    
+    """    
     Model is a simple linear relationship of sensor 1 measurements as a 
     function of sensor 2. Line is constrained to pass through the origin. 
     
